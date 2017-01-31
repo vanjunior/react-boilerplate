0ad664a091a022c0a1a3064e7a804b822f4f3a36
Ivan Junior <vanjunior89@gmail.com>
2017-01-31 18:57:11 +0700

    Change public

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from '../scss/app.scss';
 
+console.log('abc');
+
 ReactDOM.render(
 	<div>
 		<h1>React, Redux & Webpack boilerplate</h1>
